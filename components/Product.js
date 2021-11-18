21eef7ef390a7e37e92dea88589c0a833f14ac2c
Long-Pro <nvl1682@gmail.com>
2021-11-19 01:05:38 +0700

    git layout

---
+++ components/Product.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { View, Text,Image,StyleSheet,TouchableOpacity,Alert,Dimensions} from "react-native";
+export default function Product({product,onAddToCart}){
+  return (
+    <View style={styles.container}>
+      <Image
+        source={product.imageLink}
+        style={styles.image}
+      />
+      <View>
+        <Text style={styles.name}>{product.name}</Text>
+        <View style={styles.row}>
+          <Text style={styles.price}>{product.price}</Text>
+          <TouchableOpacity onPress={onAddToCart}>
+            <Text style={styles.buy}>MUA +</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+
+  )
+}
+const styles=StyleSheet.create({
+  container:{
+    borderRadius:4,
+    borderColor:'#000',
+    borderWidth:1,
+    padding:8,
+  },
+  image:{
+    width:64,
+    height:64,
+    alignSelf:'center'
+  },
+  name:{
+    //textTransform:'uppercase',
+    fontWeight:'600',
+    fontSize:18,
+    marginTop:8,
+    alignSelf:'center'
+  },
+  price:{
+    color:'#888',
+    fontSize:16
+  },
+  buy:{
+    color:'#2f95dc'
+  },
+  row:{
+    flexDirection: "row",
+    justifyContent: "space-between",
+    //flex:1
+  }
+
+})
--- screens/Category.js
+++ screens/Category.js
@@ -1,18 +1,38 @@
 import React,{useState} from 'react';
 import { StyleSheet, Text, View,ScrollView ,FlatList} from 'react-native';
-
+import Product from '../components/Product';
+const initState=[
+  { id:1, name:'product1',price:100,imageLink: require('../assets/product1.png') },
+  { id:2, name:'product2',price:200,imageLink: require('../assets/product2.png') },
+  { id:3, name:'product3',price:3000000,imageLink: require('../assets/product3.png') },
+]
 export default function Category({route}) {
-  const { name } = route.params;
+  const [products,setproducts]=useState(initState)
+  onAddToCart=()=>{
+
+  }
   return (
-      <View>
-        <Text>
-          {name}
-        </Text>
-      </View>
-  );
+    <FlatList
+      contentContainerStyle={styles.containerParent}
+      data={products}
+      numColumns={2}
+      renderItem={({item}) => 
+        <View style={styles.containerChild}>
+          <Product product={item} onAddToCart={onAddToCart} />
+        </View>
+      }
+    />
+);
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerParent: {
+    paddingHorizontal:8,
+  },
+  containerChild: {
+    paddingHorizontal:8,
+    flex:1,
+    maxWidth:'50%',
+    marginTop:16,
   },
 });
