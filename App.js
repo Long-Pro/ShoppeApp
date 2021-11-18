cdb18f96f268483b421b5644bf433ce83bf257e6
Long-Pro <nvl1682@gmail.com>
2021-11-18 13:23:57 +0700

    flatList

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import React from 'react';
-import { StyleSheet, Text, View,ScrollView } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View,ScrollView ,FlatList} from 'react-native';
 import Category from './components/Category';
 export default function App() {
+  const [categories,setCategories]=useState(
+    [
+      { id:1, name:'clothes',imageLink: require('./assets/clothes.png') },
+      { id:2, name:'hat',imageLink: require('./assets/hat.png') },
+      { id:3, name:'laptop',imageLink: require('./assets/laptop.png') },
+      { id:4, name:'pc',imageLink: require('./assets/pc.png') },
+      { id:5, name:'phone',imageLink: require('./assets/clothes.png') },
+      { id:6, name:'shoes',imageLink: require('./assets/shoes.png') },
+      { id:7, name:'sport',imageLink: require('./assets/sport.png') },
+      { id:8, name:'watch',imageLink: require('./assets/watch.png') }
+    ]
+  )
   return (
-    <View style={styles.container}>
-      <ScrollView>
-        <Category title='Phone' />
-        <Category title='Laptop' />
-        <Category title='PC' />
-        <Category title='Sport' />
-        <Category title='Clothes' />
-        <Category title='Hat' />
-        <Category title='Shoes' />
-        <Category title='Watch' />
-      </ScrollView>
-    </View>
+
+      <FlatList
+        contentContainerStyle={{padding:16}}
+        data={categories}
+        renderItem={({item}) => <Category category={item} />}
+      />
+
   );
 }
 
--- components/Category.js
+++ components/Category.js
@@ -1,43 +1,15 @@
 import React from "react";
 import { View, Text,Image,StyleSheet } from "react-native";
-const category ={
-  clothes:{
-    imageLink: require('../assets/clothes.png')
-  },
-  hat:{
-    imageLink: require('../assets/hat.png')
-  },
-  laptop:{
-    imageLink: require('../assets/laptop.png')
-  },
-  pc:{
-    imageLink: require('../assets/pc.png')
-  },
-  shoes:{
-    imageLink: require('../assets/shoes.png')
-  },
-  phone:{
-    imageLink: require('../assets/phone.png')
-  },
-  watch:{
-    imageLink: require('../assets/watch.png')
-  },
-  sport:{
-    imageLink: require('../assets/sport.png')
-  },
-}
-export default function Category(props){
-  let imageName=props.title.toLowerCase()
-  console.log(imageName)
+export default function Category({category}){
   return (
     <View style={styles.container}>
       <Text 
         style={styles.title}
       > 
-        {props.title}
+        {category.name}
       </Text>
       <Image 
-        source={category[imageName].imageLink}
+        source={category.imageLink}
         style={styles.image}
       />
     </View>
@@ -61,8 +33,8 @@ const styles=StyleSheet.create({
     //borderWidth:1
     //backgroundColor:'blue'
 
-    marginLeft:16,
-    marginRight:16,
+    // marginLeft:16,
+    // marginRight:16,
   },
   image:{
     width:64,
